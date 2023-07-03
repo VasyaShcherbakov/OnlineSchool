fde1f252ba3578266980cc30f46fe45f0a87a0ab
VasyaShcherbakov <vs.stherbakov@gmail.com>
2023-07-03 21:45:04 +0300

    43_MySQL_part_2

--- src/main/resources/HW_42.sql
+++ src/main/resources/HW_42.sql
@@ -1,54 +1,10 @@
-CREATE DATABASE "OnlineSchool"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
-----------------------------------------
-
-
-CREATE TABLE public."Students"
-(
-    id serial NOT NULL,
-    name text,
-    second_name text,
-    PRIMARY KEY (id)
-);
-
-ALTER TABLE IF EXISTS public."Students"
-    OWNER to postgres;
-
-----------------------------------------
-
-
-INSERT INTO public.students(
-	id, name, second_name)
-	VALUES (1, 'Eduard','Fedorovich');
-
-
--------------------------------------------
-
-INSERT INTO public.students(
-	id, name, second_name)
-	VALUES (1, 'Orest','Ivanovich');
-
-INSERT INTO public.students(
-	id, name, second_name)
-	VALUES (1, 'Arkady','Vadimovich');
-
-INSERT INTO public.students(
-	id, name, second_name)
-	VALUES (1, 'Makar','Eduardovich');
-
-INSERT INTO public.students(
-	id, name, second_name)
-	VALUES (1, 'Gerard','Platonovich');
-
-
-SELECT * FROM Students
 
 
 
+SELECT * FROM Students  ORDER BY second_name DESC
 
+SELECT * FROM Students  ORDER BY second_name DESC
 
+SELECT * FROM Students  ORDER BY year DESC
+
+Select count(additional_materials) from students
