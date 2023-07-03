f4cee92f77dab94704c1fabdaf1f459e0b8ef305
VasyaShcherbakov <vs.stherbakov@gmail.com>
2023-07-03 22:11:14 +0300

    43_MySQL_part_2

--- src/main/resources/HW_42.sql
+++ src/main/resources/HW_42.sql
@@ -1,10 +1,26 @@
+FROM
+    name
+    lectures_name
+        INNER JOIN orderdetails
+                   ON products.product_сode = orderdetails.product_code
+ORDER BY product_name;
 
 
 
-SELECT * FROM Students  ORDER BY second_name DESC
+FROM
+    name
+    second_name
+     year
+        INNER JOIN orderdetails
+                   ON second_name = name
+ORDER BY product_name;
 
-SELECT * FROM Students  ORDER BY second_name DESC
 
-SELECT * FROM Students  ORDER BY year DESC
-
-Select count(additional_materials) from students
+SELECT
+    lectures_name
+    cource_name
+FROM
+    name
+        RIGHT JOIN online_scool
+                   ON online_scool.lectures_name = product_сode
+ORDER BY lectures_name;
